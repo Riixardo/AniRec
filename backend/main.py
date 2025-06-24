84900d94e9abcfd2775779b7c4e9384a41e98885
Riixardo <RicardoMonriguez2@gmail.com>
2025-06-24 00:31:59 -0700

    changed uvicorn run command

--- backend/main.py
+++ backend/main.py
@@ -177,4 +177,10 @@ def read_root():
     return {"message": "Hello from AniRec API"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(
+        app, 
+        host="0.0.0.0", 
+        port=8000,
+        proxy_headers=True,
+        forwarded_allow_ips='127.0.0.1'
+    )
