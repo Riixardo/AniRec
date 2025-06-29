9817cb919314d30d0a46591a02a43f723265756e
Riixardo <RicardoMonriguez2@gmail.com>
2025-06-29 11:38:28 -0700

    google analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,6 +8,24 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      {/* Load the GA script from Google */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-LRXXP8R8JY"
+        strategy="afterInteractive"
+      />
+      {/* Inline GA initialization script */}
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LRXXP8R8JY');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
